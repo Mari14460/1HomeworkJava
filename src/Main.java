1d4d5d6a9ab8aaaecb6acf1131f9b3a913b2a427
Marina Zinchenko <amanda79116@gmail.com>
2022-09-16 20:01:44 +0300

    clean-up

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-
-
+            
             int eaters = 5; // сколько людей будут есть
 
             int water = 3000; // миллилитров воды
@@ -15,7 +13,5 @@ public class Main {
             System.out.println((potatoes / eaters) + " картофелин(а)");
             System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
             System.out.println((spices / eaters) + " ложек(ка) специй");
-
-
     }
 }
